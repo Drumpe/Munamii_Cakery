7d0d3d9a9514edf7ad936567e5d67198183b7785
Ola Persson <drumpert@gmail.com>
2025-06-25 11:51:44 +0200

    Refactor App component to use BrowserRouter directly from react-router

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,11 @@
-import { BrowserRouter as Router } from "react-router-dom";
+import { BrowserRouter } from "react-router";
 import AppRoutes from "./AppRoutes";
 
 function App() {
   return (
-    <Router>
+    <BrowserRouter>
       <AppRoutes />
-    </Router>
+    </BrowserRouter>
   );
 }
 
